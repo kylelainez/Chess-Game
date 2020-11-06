b6d523ca32b4c9fdb366ba3edb1dea32946d7cb7
kylelainez <kyleadrianlainez@gmail.com>
2020-11-05 16:28:28 -0800

    Add Sound Effect

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const backRow = [
 	'knight',
 	'rook'
 ];
-
+const soundEffect = new Audio('../soundEffects/sound.mp3');
 //* ------------------------------------- State Variables -------------------------------------
 let boardPieces;
 let chessBoard;
@@ -590,6 +590,7 @@ const movePieces = (i, j) => {
 					});
 					renderTurnMessage(boardPieces[i][j].side);
 					winLoseCheck();
+					soundEffect.play();
 				}
 			});
 
